b1b3583d0487337a4856bca66e2d4c4cc5cd26ec
PauloSouza-GV <paulosouza@grupovila.com.br>
2020-04-07 09:57:39 -0300

    Substituindo para property para facilitar as validações posteriores

--- ByteBank/01-ByteBank/Cliente.cs
+++ ByteBank/01-ByteBank/Cliente.cs
@@ -5,22 +5,29 @@ namespace ByteBank
 {
     class Cliente
     {
-        public int id;
-        public string nome;
-        protected string cgc;
-        protected DateTime dtNascimento;
-        protected DateTime dtRegistro;
-        protected int idade;
-
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cgc { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataRegistro { get; set; }
+        public int Idade
+        {
+            get
+            {
+                return new Utilidades().GetIdade(DataNascimento);
+            }
+            set
+            {
+            }
+        }
 
         public Cliente(string nome, string cgc, DateTime dtNascimento)
         {
-            this.id++;/*GetMax - inicialmente será usado somente 0-1*/;
-            this.nome = nome ?? throw new ArgumentNullException(nameof(nome));
-            this.cgc = cgc;
-            this.dtNascimento = dtNascimento;
-            this.dtRegistro = DateTime.Now.ToLocalTime();
-            this.idade = new Utilidades().GetIdade(dtNascimento);
+            Id++;/*GetMax - inicialmente será usado somente 0-1*/;
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Cgc = cgc ?? throw new ArgumentNullException(nameof(cgc));
+            DataNascimento = dtNascimento;
+            DataRegistro = DateTime.Now.ToLocalTime();
         }
 
     }
--- ByteBank/01-ByteBank/ContaCorrente.cs
+++ ByteBank/01-ByteBank/ContaCorrente.cs
@@ -4,14 +4,29 @@ namespace ByteBank
 {
     class ContaCorrente
     {
-        public Cliente cliente;
-        public int Agencia;
-        public int Conta;
-        public double saldo;
+        public Cliente Cliente { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        private double _saldo = 0.1;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
 
         public ContaCorrente(Cliente cliente, int agencia, int conta)
         {
-            this.cliente = cliente;
+            Cliente = cliente;
             Agencia = agencia;
             Conta = conta;
         }
