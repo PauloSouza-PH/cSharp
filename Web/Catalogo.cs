ab0651846fd7725e8da21b9ea010ef6c49a1c2d2
PauloSouza-GV <paulosouza@grupovila.com.br>
2020-04-22 17:21:26 -0300

    Testes de catalogos web

---
+++ Web/Catalogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Desconhecido", 10.29m));
+            livros.Add(new Livro("002", "Qualquer 1", 12.29m));
+
+            return livros;
+        }
+    }
+}
---
+++ Web/Relatorio.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Relatorio : IRelatorio
+    {
+        public Relatorio(ICatalogo catalogo)
+        {
+            this.catalogo = catalogo;
+        }
+
+        public async Task Imprimir(HttpContext context)
+        {
+            foreach (Livro item in catalogo.GetLivros())
+            {
+                await context.Response.WriteAsync(item.ToString());
+            }
+        }
+
+        public ICatalogo catalogo { get; }
+    }
+}
